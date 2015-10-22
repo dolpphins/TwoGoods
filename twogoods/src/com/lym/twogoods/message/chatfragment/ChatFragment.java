81aa769fa08a851bd783b1d177fee4522ab0ba88
yaojunfeng <2842697714@qq.com>
2015-10-22 19:00:12 +0800

    continue develop my part

--- twogoods/src/com/lym/twogoods/message/chatfragment/ChatFragment.java
+++ twogoods/src/com/lym/twogoods/message/chatfragment/ChatFragment.java
@@ -1,6 +1,12 @@
 package com.lym.twogoods.message.chatfragment;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.View.OnTouchListener;
+import android.view.inputmethod.InputMethodManager;
 
 import com.lym.twogoods.R;
 import com.lym.twogoods.fragment.base.PullListFragment;
@@ -15,9 +21,39 @@ public class ChatFragment extends PullListFragment{
 	}
 
 	private void initView() {
-		
-		setMode(Mode.BOTH);
+		initXlistView();
+	}
+
+	private void initXlistView() {
+		// TODO 自动生成的方法存根
+		setMode(Mode.PULLDOWN);
 		setAdapter();
+		
+		mListView.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				// TODO Auto-generated method stub
+				hideSoftInputView();
+				
+				return false;
+			}
+		});
+	}
+	
+	
+	/*
+	 * 隐藏软键盘
+	 * 
+	 */
+	protected void hideSoftInputView() {
+		InputMethodManager manager = ((InputMethodManager)getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE));
+		if (getActivity().getWindow().getAttributes().softInputMode != WindowManager.
+				LayoutParams.SOFT_INPUT_STATE_HIDDEN) {
+			if (getActivity().getCurrentFocus() != null)
+				manager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 
+						InputMethodManager.HIDE_NOT_ALWAYS);
+		}
 	}
 
 	private void setAdapter() {
--- twogoods/src/com/lym/twogoods/message/ui/ChatActivity.java
+++ twogoods/src/com/lym/twogoods/message/ui/ChatActivity.java
@@ -2,11 +2,16 @@ package com.lym.twogoods.message.ui;
 
 import com.lym.twogoods.R;
 import com.lym.twogoods.message.chatfragment.ChatFragment;
+import com.lym.twogoods.message.view.EmoticonsEditText;
 import com.lym.twogoods.ui.base.BottomDockBackFragmentActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +30,7 @@ public class ChatActivity extends BottomDockBackFragmentActivity{
 	
 	private LinearLayout moreLinearLayout,emoLinearLayout,voiceLinearLayout,addLinearLayout;
 	
+	private EmoticonsEditText edit_user_comment;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自动生成的方法存根
@@ -44,6 +50,8 @@ public class ChatActivity extends BottomDockBackFragmentActivity{
 		emoLinearLayout = (LinearLayout) bottomView.findViewById(R.id.chat_layout_emo_content);
 		voiceLinearLayout = (LinearLayout) bottomView.findViewById(R.id.chat_layout_voice_content);
 		addLinearLayout = (LinearLayout) bottomView.findViewById(R.id.chat_layout_add_content);
+		
+		edit_user_comment = (EmoticonsEditText) findViewById(R.id.message_chat_edit_user_comment);
 	}
 
 
@@ -55,25 +63,79 @@ public class ChatActivity extends BottomDockBackFragmentActivity{
 		return bottomView;
 	}
 	
-	
 	public void toAction(View view)
 	{
-		Toast.makeText(this, "哈哈哈哈哈", Toast.LENGTH_LONG).show();
-		moreLinearLayout.setVisibility(View.VISIBLE);
+		
 		switch(view.getId()){
-		case R.id.message_chat_btn_add:
-			if(emoLinearLayout.getVisibility()==View.VISIBLE)
+		case R.id.message_chat_edit_user_comment:
+			if(moreLinearLayout.getVisibility()==View.VISIBLE){
+				moreLinearLayout.setVisibility(View.GONE);
 				emoLinearLayout.setVisibility(View.GONE);
-			addLinearLayout.setVisibility(View.VISIBLE);
+				addLinearLayout.setVisibility(View.GONE);
+			}
+			break;
+		case R.id.message_chat_btn_add:
+			if (moreLinearLayout.getVisibility() == View.GONE) {
+				moreLinearLayout.setVisibility(View.VISIBLE);
+				addLinearLayout.setVisibility(View.VISIBLE);
+				emoLinearLayout.setVisibility(View.GONE);
+				hideSoftInputView();
+			} else {
+				if (emoLinearLayout.getVisibility() == View.VISIBLE) {
+					emoLinearLayout.setVisibility(View.GONE);
+					addLinearLayout.setVisibility(View.VISIBLE);
+				} else {
+					((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
+					.showSoftInput(edit_user_comment, 0);
+					moreLinearLayout.setVisibility(View.GONE);
+				}
+			}
 			break;
 		case R.id.message_chat_btn_emo:
-			if(addLinearLayout.getVisibility()==View.VISIBLE)
-				addLinearLayout.setVisibility(View.GONE);
-			emoLinearLayout.setVisibility(View.VISIBLE);
+			if (moreLinearLayout.getVisibility() == View.GONE) {
+				showEditEmoState(true);
+			} else {
+				if (addLinearLayout.getVisibility() == View.VISIBLE) {
+					addLinearLayout.setVisibility(View.GONE);
+					emoLinearLayout.setVisibility(View.VISIBLE);
+				} else {
+					((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
+					.showSoftInput(edit_user_comment, 0);
+					moreLinearLayout.setVisibility(View.GONE);
+				}
+			}
+			break;
+		case R.id.message_chat_tv_location:
+			Toast.makeText(this, "点击了发送位置", Toast.LENGTH_LONG).show();
+			break;
+		case R.id.message_chat_tv_picture:
+			Toast.makeText(this, "点击了发送图片", Toast.LENGTH_LONG).show();
+			break;
+		case R.id.message_chat_tv_voice:
+			Toast.makeText(this, "点击了发送声音", Toast.LENGTH_LONG).show();
+			break;
+		default:
 			break;
 		}
 	}
 	
+	private void showEditEmoState(boolean isEmo) {
+		// TODO 自动生成的方法存根
+		edit_user_comment.requestFocus();
+		if (isEmo) {
+			moreLinearLayout.setVisibility(View.VISIBLE);
+			//layout_more.setVisibility(View.VISIBLE);
+			emoLinearLayout.setVisibility(View.VISIBLE);
+			addLinearLayout.setVisibility(View.GONE);
+			hideSoftInputView();
+		} else {
+			moreLinearLayout.setVisibility(View.GONE);
+			showSoftInputView();
+		}
+	}
+
+
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		
@@ -83,7 +145,32 @@ public class ChatActivity extends BottomDockBackFragmentActivity{
 		    moreLinearLayout.setVisibility(View.GONE);
 			return true;
 		}
+		
 		return super.onKeyDown(keyCode, event);
+	}
+	
+	
+	/*
+	 * 隐藏软键盘
+	 * 
+	 */
+	public void hideSoftInputView() {
+		InputMethodManager manager = ((InputMethodManager)this.getSystemService(Activity.INPUT_METHOD_SERVICE));
+		if (getWindow().getAttributes().softInputMode != WindowManager.
+				LayoutParams.SOFT_INPUT_STATE_HIDDEN) {
+			if (getCurrentFocus() != null)
+				manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 
+						InputMethodManager.HIDE_NOT_ALWAYS);
+		}
+	}
+	
+	// 显示软键盘
+	public void showSoftInputView() {
+		if (getWindow().getAttributes().softInputMode == WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN) {
+			if (getCurrentFocus() != null)
+				((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE))
+						.showSoftInput(edit_user_comment, 0);
+		}
 	}
 
 }
